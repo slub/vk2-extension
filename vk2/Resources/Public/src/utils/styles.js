2212e2a2f804c92adb3202b1e26a17f5ee98e399
Jacob Mendt <jacobmendt@googlemail.com>
2015-10-06 10:43:57 +0200

    Update point styles of georeference map

--- vk2/Resources/Public/src/utils/styles.js
+++ vk2/Resources/Public/src/utils/styles.js
@@ -55,53 +55,31 @@ vk2.utils.Styles.GEOREFERENCE_CLIP_POLYGON = new ol.style.Style({
 });
 
 /**
- * @type {Object}
- */
-vk2.utils.Styles.GEOREFERENCE_POINT = new ol.style.Style({
-	'image': new ol.style.Circle({
-		'radius': 7,
-		'fill': new ol.style.Fill({
-			'color': 'rgba(255, 255, 255, 0.6)'
-		}),
-		'stroke': new ol.style.Stroke({
-			'color': '#319FD3',
-			'width': 1.5
-		})
-	}),
-	'text': new ol.style.Text({
-	    textAlign: 'start',
-	    textBaseline: 'bottom',
-		font: '12px Calibri,sans-serif',
-		text: 'Test',
-	    fill: new ol.style.Fill({color: '#aa3300'}),
-	    stroke: new ol.style.Stroke({color: '#ffffff', width: 3}),
-	    offsetX: 10,
-	    offsetY: -5
-	})
-});
-
-/**
  * @param {string=} opt_text
  * @return {ol.style.Style}
  */
 vk2.utils.Styles.getGeoreferencePointStyle = function(opt_text){
-	var text = goog.isDef(opt_text) ? opt_text : undefined;
+	var radius = 8,
+		dash = 2*Math.PI*radius/6,
+		dash = [ 0, dash, dash, dash, dash, dash, dash ],
+		text = goog.isDef(opt_text) ? opt_text : undefined;
 	return new ol.style.Style({
-		'image': new ol.style.Circle({
-			'radius': 7,
-			'fill': new ol.style.Fill({
-				'color': 'rgba(255, 255, 255, 0.6)'
+		image: new ol.style.Circle({
+			radius: radius,
+			fill: new ol.style.Fill({
+				color: 'rgba(255,255,255,0.6)'
 			}),
-			'stroke': new ol.style.Stroke({
-				'color': '#319FD3',
-				'width': 1.5
+			stroke: new ol.style.Stroke({
+				color: 'rgba(49,159,211,0.5)',
+				width: 15,
+				lineDash: dash
 			})
 		}),
-		'text': new ol.style.Text({
+		text: new ol.style.Text({
 		    textAlign: 'start',
 		    textBaseline: 'bottom',
 			font: '12px Calibri,sans-serif',
-			'text': text,
+			text: text,
 		    fill: new ol.style.Fill({color: '#aa3300'}),
 		    stroke: new ol.style.Stroke({color: '#ffffff', width: 3}),
 		    offsetX: 10,
@@ -148,24 +126,27 @@ vk2.utils.Styles.GEOREFERENCE_POINT_HOVER = new ol.style.Style({
  * @return {ol.style.Style}
  */
 vk2.utils.Styles.getGeoreferencePointHover = function(opt_text){
-	var text = goog.isDef(opt_text) ? opt_text : undefined;
+	var radius = 11,
+		dash = 2*Math.PI*radius/6,
+		dash = [ 0, dash, dash, dash, dash, dash, dash ],
+		text = goog.isDef(opt_text) ? opt_text : undefined;
 	return new ol.style.Style({
-		'image': new ol.style.Circle({
-			'radius': 11,
-		    'fill': new ol.style.Fill({
-		    	'color': 'rgba(255,0,0,0.1)'
-		    }),
-		    'stroke': new ol.style.Stroke({
-		    	'color': '#f00',
-		    	'width': 2
-		    })
+		image: new ol.style.Circle({
+			radius: radius,
+			fill: new ol.style.Fill({
+				color: 'rgba(255,128,0,0.6)'
+			}),
+			stroke: new ol.style.Stroke({
+				color: 'rgba(240,0,0,0.5)',
+				width: 15,
+				lineDash: dash
+			})
 		}),
-		'zIndex': 100000,
-		'text': new ol.style.Text({
+		text: new ol.style.Text({
 		    textAlign: 'start',
 		    textBaseline: 'bottom',
 			font: '12px Calibri,sans-serif',
-			'text': text,
+			text: text,
 		    fill: new ol.style.Fill({color: '#aa3300'}),
 		    stroke: new ol.style.Stroke({color: '#ffffff', width: 3}),
 		    offsetX: 10,
