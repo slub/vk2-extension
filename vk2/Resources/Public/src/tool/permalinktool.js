01449f7928f139e4c48eb3fd5b90e064317bb19c
Jacob Mendt <jacobmendt@googlemail.com>
2015-11-25 14:59:05 +0100

    Support backwards compatibility through dataid field

--- vk2/Resources/Public/src/tool/permalinktool.js
+++ vk2/Resources/Public/src/tool/permalinktool.js
@@ -20,6 +20,13 @@ vk2.tool.PermalinkEventType = {
 };
 
 /**
+ * The permalink tools offers functions for parsing permalinks and creating them. It uses the following url query
+ * parameters:
+ * 		z: zoom level
+ * 		c: center coordinate in EPSG:4326
+ * 		oid: object id notation of the virtual map forum 2.0
+ * 		dataid: dataid to support linking throught other slub services.
+ *
  * @constructor
  * @extends {goog.events.EventTarget}
  */
@@ -34,91 +41,95 @@ goog.inherits(vk2.tool.Permalink, goog.events.EventTarget);
  * @param {ol.Map} map
  */
 vk2.tool.Permalink.prototype.parsePermalink = function(map){
+
 	var uri = new goog.Uri(window.location.href),
-		queryData = uri.getQueryData(),
+		params = uri.getQueryData(),
 		center,
 		// default zoom
 		zoom = 4;
 		
-	// if there are information to center and zoom parse them
-	if (queryData.containsKey('z') && queryData.containsKey('c')){
-		// zoom to given center
-		var centerArr = queryData.get('c').split(',');
-		center = ol.proj.transform([parseFloat(centerArr[0], 0),parseFloat(centerArr[1], 0)], 'EPSG:4326', vk2.settings.MAPVIEW_PARAMS['projection']);
-		zoom = parseInt(queryData.get('z'), 0);
-	};
-	
-	// if there is center information but not oid zoom to center
-	if ((!queryData.containsKey('oid') || queryData.get('oid') === "") && goog.isDef(center)){
-		this.zoomToMapView_(map, center, zoom);
-		return;
-	};
-	
-	// no oid so return
-	if (!queryData.containsKey('oid') || queryData.get('oid') === "")
-		return;
-	
-	// else parse the information regarding the oid
-	var objectids = queryData.get('oid').split(',');
-			
-	// remove empty strings
-	for (var i = 0; i < objectids.length; i++){
-		if (objectids[i] == '')
-			objectids.splice(i, 1);
-	};			
-	objectids.reverse();
-	
-	if (objectids.length === 1 && !goog.isDef(center)) {
-		var url = vk2.request.ElasticSearch.getFeatureForId('map', objectids[0]);
-		// send request
-		goog.net.XhrIo.send(url, goog.bind(function(e) {
-			var xhr = /** @type {goog.net.XhrIo} */ (e.target);
-			var data = xhr.getResponseJson() ? xhr.getResponseJson() : undefined;
-	    	xhr.dispose();
+	// if there are information to center always zoom to it
+	if (params.containsKey('c')){
+		var centerArray = params.get('c').split(',');
+		center = ol.proj.transform([parseFloat(centerArray[0], 0),parseFloat(centerArray[1], 0)], 'EPSG:4326',
+			vk2.settings.MAPVIEW_PARAMS['projection']);
+		zoom = parseInt(params.get('z'), 0);
 
-	    	if (goog.isDef(data)) {
-	    		var feature = vk2.parser.ElasticSearch.readFeature(objectids[0], data['_source'],
-	    				vk2.settings.ELASTICSEARCH_SRS, 'EPSG:3857');
-	    		
-	    		if (feature !== undefined) {
-	    			center = feature.getGeometry().getInteriorPoint().getCoordinates();
-	    			this.dispatchEvent(new goog.events.Event(vk2.tool.PermalinkEventType.ADDMAP,{'feature':feature}));
-		    		this.zoomToMapView_(center, zoom);
-	    		};	    			
-	    	};
-		}, this), 'GET');
-	} else {
+		// zoom to center
+		this.zoomToMapView_(map, center, zoom);
+	};
+
+	/**
+	 * Function for parsing and adding the response
+	 * @param {Array.<Object>} data
+	 * @param {Array.<string>|undefined} opt_objectids
+ 	 */
+	var parseAddFeatures = goog.bind(function(data, opt_objectids){
+
+		var features = vk2.parser.ElasticSearch.readFeatures(data,
+			vk2.settings.ELASTICSEARCH_SRS, 'EPSG:3857');
+
+		if (opt_objectids !== undefined) {
+			// dispatch addmtb events, but dispatch them in the correct ording corresponding to the
+			// ordering of the objectids array
+			for (var i = 0; i < objectids.length; i++){
+				for (var j = 0; j < features.length; j++){
+					if (objectids[i] == features[j].getId())
+						this.dispatchEvent(new goog.events.Event(vk2.tool.PermalinkEventType.ADDMAP,{'feature':features[j]}));
+				};
+			};
+		} else {
+			for (var j = 0; j < features.length; j++){
+				this.dispatchEvent(new goog.events.Event(vk2.tool.PermalinkEventType.ADDMAP,{'feature':features[j]}));
+			};
+		}
+
+		// if no center was set so far get center of the first map
+		if (!center && features.length > 0) {
+			center = features[0].getGeometry().getInteriorPoint().getCoordinates();};
+		this.zoomToMapView_(map, center, zoom);
+
+	}, this);
+
+	// add map objects
+	if (params.containsKey('oid') && params.get('oid') !== "") {
+		// native oid support for virtual map forum ids
+
+		// parse oids
+		var objectids = params.get('oid').split(',');
+
+		// remove empty strings
+		for (var i = 0; i < objectids.length; i++){
+			if (objectids[i] == '')
+				objectids.splice(i, 1);
+		};
+		objectids.reverse();
+
 		// assumes that there exists multiple oid regaring a permalink
-		vk2.request.ElasticSearch.getFeatureForIds('map', objectids, goog.bind(function(e){
+		vk2.request.ElasticSearch.getFeatureForIds('map', objectids, function(e) {
 			var xhr = /** @type {goog.net.XhrIo} */ (e.target);
 			var data = xhr.getResponseJson() ? xhr.getResponseJson() : undefined;
-	    	xhr.dispose();
-	    	
-	    	if (goog.isDef(data)) {
-	    		var features = vk2.parser.ElasticSearch.readFeatures(data['docs'],
-	    				vk2.settings.ELASTICSEARCH_SRS, 'EPSG:3857');
-	    				    		
-		    	// dispatch addmtb events, but dispatch them in the correct ording corresponding to the 
-		    	// ordering of the objectids array
-		    	for (var i = 0; i < objectids.length; i++){
-			    	for (var j = 0; j < features.length; j++){
-			    		if (objectids[i] == features[j].getId())
-			    			this.dispatchEvent(new goog.events.Event(vk2.tool.PermalinkEventType.ADDMAP,{'feature':features[j]}));
-			    	};
-		    	};
-		    	
-		    	// if no center was set so far get center of the first map
-		    	if (!center && features.length > 0) {
-		    		center = features[0].getGeometry().getInteriorPoint().getCoordinates();
-		    	};
-	    		this.zoomToMapView_(center, zoom);
-	    	} else {
-	    		if (goog.DEBUG) {
-	    			alert('Could not resolve permalink.');
-	    		};
-	    	};
-		}, this));
-	};
+			xhr.dispose();
+
+			if (data['docs'] !== undefined && data['docs'].length > 0) {
+				parseAddFeatures(data['docs'], objectids);
+			};
+		});
+	} else if (params.containsKey('dataid') && params.get('dataid') !== "") {
+		// assumes that there exists multiple oid regaring a permalink
+		// send request
+		var url = vk2.settings.ELASTICSEARCH_NODE + '/_search',
+			payload = vk2.request.ElasticSearch.getFeaturesForIdsFilterQuery('dataid', [params.get('dataid')]);
+		goog.net.XhrIo.send(url, function(e) {
+			var xhr = /** @type {goog.net.XhrIo} */ (e.target);
+			var data = xhr.getResponseJson() ? xhr.getResponseJson() : undefined;
+			xhr.dispose();
+
+			if (data['hits'] !== undefined && data['hits']['hits'] !== undefined && data['hits']['hits'].length > 0) {
+				parseAddFeatures(data['hits']['hits']);
+			};
+		}, 'POST', JSON.stringify(payload));
+	}
 };
 
 /**
@@ -144,13 +155,13 @@ vk2.tool.Permalink.createPermalink = function(map){
 		
 	// create permalink
 	var permalink = new goog.Uri(window.location.origin + vk2.utils.routing.getBaseUrl() + '&welcomepage=off');
-	var qData = permalink.getQueryData();
+	var params = permalink.getQueryData();
 		
 	// append zoom, center and objectids to queryData
-	qData.set('z',zoom);
-	qData.set('c',vk2.utils.round(center[0], 4) + ',' + vk2.utils.round(center[1], 4));
-	qData.set('oid', objectids);
-	permalink.setQueryData(qData);
+	params.set('z',zoom);
+	params.set('c',vk2.utils.round(center[0], 4) + ',' + vk2.utils.round(center[1], 4));
+	params.set('oid', objectids);
+	permalink.setQueryData(params);
 		
 	if (goog.DEBUG){
 		console.log(objectids);
