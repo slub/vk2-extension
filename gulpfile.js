7d5199792190ec35bcf63b5b006cb9cf1884754b
Jacob Mendt <jacobmendt@googlemail.com>
2016-06-22 14:40:02 +0200

    Add simple build to library

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,32 @@ gulp.task('minify-css', function() {
 });
 
 /**
+ * Build of library with google closure compiler - simple optimization
+ */
+gulp.task('js-compile-simple', function() {
+	return gulp.src(['Resources/Public/vk2-require.js', 'Resources/Public/src/**/*.js', 'Resources/Public/lib/closure-library/closure/goog/**/*.js'])
+	.pipe(closureCompiler({
+		compilerPath: 'node_modules/closure-compiler/node_modules/google-closure-compiler/compiler.jar',
+		fileName: targetDir + 'vk2.js',
+		compilerFlags: {
+			closure_entry_point: 'vk2.require',
+			compilation_level: 'SIMPLE',
+			only_closure_dependencies: true,
+			generate_exports: true,
+			define: [
+				'goog.DEBUG=false'
+			],
+			externs: closureExterns,
+			output_wrapper: '(function(){%output%}).call(window);',
+			warning_level: 'VERBOSE'
+		}
+	}))
+	.pipe(gulp.dest('Resources/Public')
+	);
+});
+
+
+/**
  * Build of library with google closure compiler - advanced optimization (debug)
  */
 gulp.task('js-compile-advanced-debug', function() {
@@ -83,4 +109,4 @@ gulp.task('js-compile-advanced', function() {
     );
 });
 
-gulp.task('default', ['minify-css', 'js-compile-advanced-debug', 'js-compile-advanced']);
+gulp.task('default', ['minify-css', 'js-compile-simple', 'js-compile-advanced-debug', 'js-compile-advanced']);
